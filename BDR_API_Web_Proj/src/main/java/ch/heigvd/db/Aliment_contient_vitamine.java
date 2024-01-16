dff53dada068d407cd9c11afebfffee3f53eaded
lucaslattion <71433770+lucaslattion@users.noreply.github.com>
2024-01-16 11:18:38 +0100

    add backend routes and documentation

--- BDR_API_Web_Proj/src/main/java/ch/heigvd/db/Aliment_contient_vitamine.java
+++ BDR_API_Web_Proj/src/main/java/ch/heigvd/db/Aliment_contient_vitamine.java
@@ -2,7 +2,7 @@ package ch.heigvd.db;
 
 public class Aliment_contient_vitamine {
     public String anom;
-    public String kcal;
+    public String vinom;
     public double quantite;
     public Aliment_contient_vitamine() {
     }
