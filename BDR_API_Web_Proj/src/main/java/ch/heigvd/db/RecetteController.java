60cf39e630595193dfc718d33793b3d104d7d964
AlanSottile <alan.sottile@heig-vd.ch>
2024-01-21 14:31:15 +0100

    adding changes

--- BDR_API_Web_Proj/src/main/java/ch/heigvd/db/RecetteController.java
+++ BDR_API_Web_Proj/src/main/java/ch/heigvd/db/RecetteController.java
@@ -2,10 +2,7 @@ package ch.heigvd.db;
 
 import io.javalin.http.*;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -102,6 +99,33 @@ public class RecetteController {
         throw new UnauthorizedResponse();
     }
 
+    public void getOne(Context ctx) throws SQLException {
+        String nomRecette = ctx.pathParam("rnom");
+
+        if (authController.validLoggedUser(ctx)) {
+            // Vous devez remplacer 'conn' par votre connexion à la base de données
+
+                String query = "SELECT * FROM recette WHERE rnom = ?";
+                try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                    stmt.setString(1, nomRecette);
+
+                    ResultSet rs = stmt.executeQuery();
+
+                    if (rs.next()) {
+                        Recette recette = new Recette();
+                        recette.rnom = rs.getString("rnom");
+                        recette.instructions = rs.getString("instructions");
+                        recette.type_recette = rs.getString("type_recette");
+
+                        ctx.json(recette);
+                        return;
+                    }
+                }
+
+                ctx.status(404).json(new Error("Recette non trouvée"));
+            }
+    }
+
     public void create(Context ctx) throws SQLException {
         if(authController.validLoggedUser(ctx)){
 
@@ -178,5 +202,119 @@ public class RecetteController {
             return;
         }
         throw new UnauthorizedResponse();
+    }
+
+    public class InformationsResponse {
+        public String recette;
+        public double calories;
+        public double proteines;
+        public double glucides;
+        public double lipides;
+        public double fibres;
+        public double sodium;
+        public double vitamineA;
+        public double vitamineB1;
+        public double vitamineB2;
+        public double vitamineB3;
+        public double vitamineB6;
+        public double vitamineB12;
+        public double vitamineC;
+        public double vitamineD;
+        public double vitamineE;
+
+        public InformationsResponse(String recette, double calories, double proteines, double glucides, double lipides,
+                                    double fibres, double sodium, double vitamineA, double vitamineB1, double vitamineB2,
+                                    double vitamineB3, double vitamineB6, double vitamineB12, double vitamineC,
+                                    double vitamineD, double vitamineE) {
+            this.recette = recette;
+            this.calories = calories;
+            this.proteines = proteines;
+            this.glucides = glucides;
+            this.lipides = lipides;
+            this.fibres = fibres;
+            this.sodium = sodium;
+            this.vitamineA = vitamineA;
+            this.vitamineB1 = vitamineB1;
+            this.vitamineB2 = vitamineB2;
+            this.vitamineB3 = vitamineB3;
+            this.vitamineB6 = vitamineB6;
+            this.vitamineB12 = vitamineB12;
+            this.vitamineC = vitamineC;
+            this.vitamineD = vitamineD;
+            this.vitamineE = vitamineE;
+        }
+    }
+
+    public void getInformations(Context ctx) throws SQLException {
+        String nomRecette = ctx.pathParam("rnom");
+
+        if (authController.validLoggedUser(ctx)) {
+            // Vous devez remplacer 'conn' par votre connexion à la base de données
+
+            String query = "SELECT " +
+                    "r.rnom AS Recette, " +
+                    "SUM(a.kcal) AS Calories, " +
+                    "SUM(a.proteines) AS Proteines, " +
+                    "SUM(a.glucides) AS Glucides, " +
+                    "SUM(a.lipides) AS Lipides, " +
+                    "SUM(a.fibres) AS Fibres, " +
+                    "SUM(a.sodium) AS Sodium, " +
+                    "SUM(aav.quantite) AS Vitamine_A, " +
+                    "SUM(ab1v.quantite) AS Vitamine_B1, " +
+                    "SUM(ab2v.quantite) AS Vitamine_B2, " +
+                    "SUM(ab3v.quantite) AS Vitamine_B3, " +
+                    "SUM(ab6v.quantite) AS Vitamine_B6, " +
+                    "SUM(ab12v.quantite) AS Vitamine_B12, " +
+                    "SUM(acv.quantite) AS Vitamine_C, " +
+                    "SUM(adv.quantite) AS Vitamine_D, " +
+                    "SUM(aev.quantite) AS Vitamine_E " +
+                    "FROM Recette r " +
+                    "INNER JOIN Recette_contient_Aliment rca ON r.rnom = rca.rnom " +
+                    "INNER JOIN Aliment a ON rca.anom = a.anom " +
+                    "LEFT JOIN Aliment_contient_Vitamine aav ON a.anom = aav.anom AND aav.vinom = 'Vitamine A' " +
+                    "LEFT JOIN Aliment_contient_Vitamine ab1v ON a.anom = ab1v.anom AND ab1v.vinom = 'Vitamine B1' " +
+                    "LEFT JOIN Aliment_contient_Vitamine ab2v ON a.anom = ab2v.anom AND ab2v.vinom = 'Vitamine B2' " +
+                    "LEFT JOIN Aliment_contient_Vitamine ab3v ON a.anom = ab3v.anom AND ab3v.vinom = 'Vitamine B3' " +
+                    "LEFT JOIN Aliment_contient_Vitamine ab6v ON a.anom = ab6v.anom AND ab6v.vinom = 'Vitamine B6' " +
+                    "LEFT JOIN Aliment_contient_Vitamine ab12v ON a.anom = ab12v.anom AND ab12v.vinom = 'Vitamine B12' " +
+                    "LEFT JOIN Aliment_contient_Vitamine acv ON a.anom = acv.anom AND acv.vinom = 'Vitamine C' " +
+                    "LEFT JOIN Aliment_contient_Vitamine adv ON a.anom = adv.anom AND adv.vinom = 'Vitamine D' " +
+                    "LEFT JOIN Aliment_contient_Vitamine aev ON a.anom = aev.anom AND aev.vinom = 'Vitamine E' " +
+                    "WHERE r.rnom = ? " +
+                    "GROUP BY r.rnom";
+
+            try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                stmt.setString(1, nomRecette);
+
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    InformationsResponse informations = new InformationsResponse(
+                            rs.getString("Recette"),
+                            rs.getDouble("Calories"),
+                            rs.getDouble("Proteines"),
+                            rs.getDouble("Glucides"),
+                            rs.getDouble("Lipides"),
+                            rs.getDouble("Fibres"),
+                            rs.getDouble("Sodium"),
+                            rs.getDouble("Vitamine_A"),
+                            rs.getDouble("Vitamine_B1"),
+                            rs.getDouble("Vitamine_B2"),
+                            rs.getDouble("Vitamine_B3"),
+                            rs.getDouble("Vitamine_B6"),
+                            rs.getDouble("Vitamine_B12"),
+                            rs.getDouble("Vitamine_C"),
+                            rs.getDouble("Vitamine_D"),
+                            rs.getDouble("Vitamine_E")
+                    );
+
+                    // Vous pouvez ajuster la réponse JSON en fonction de votre besoin
+                    ctx.json(informations);
+                    return;
+                }
+            }
+
+            ctx.status(404).json(new Error("Recette non trouvée"));
+        }
     }
 }
